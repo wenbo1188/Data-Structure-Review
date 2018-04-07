9f03b504350481dae07d6df436395ac93c6376ac
Wen Bo <15026630170@163.com>
2018-04-07 10:20:29 +0800

    [linear-table] add function
    add function:
    LOCATE(L, X)
    LENGTH(L)
    link_two_list
    
    Signed-off-by: Wen Bo <15026630170@163.com>

--- linear_table/linklist.c
+++ linear_table/linklist.c
@@ -66,3 +66,89 @@ void linklist_traverse(linklist head)
 
 	printf("\n");
 }
+
+int linklist_locate(linklist head, int elem)
+{
+	linklist p = NULL;
+	int pos = 0;
+
+	if (!head)
+	{
+		printf("head is null\n");
+		return -1;
+	}
+
+	p = head->next;
+	while (p)
+	{
+		pos++;
+
+		if (p->value == elem)
+		{
+			return pos;
+		}
+
+		p = p->next;
+	}
+
+	return -1;
+}
+
+int linklist_length(linklist head)
+{
+	linklist p = NULL;
+	int length = 0;
+
+	if (!head)
+	{
+		printf("head is null\n");
+		return 0;
+	}
+
+	p = head->next;
+	while (p)
+	{
+		length++;
+		p = p->next;
+	}
+
+	return length;
+}
+
+int linklist_link_two_list(linklist ha, int la, linklist hb, int lb, linklist *hc)
+{
+	linklist first = NULL;
+	linklist second = NULL;
+	linklist p = NULL;
+
+	if (!ha || !hb)
+	{
+		printf("ha or hb is null\n");
+		return -1;
+	}
+
+	if (la <= lb)
+	{
+		first = ha;
+		second = hb;
+	}
+	else
+	{
+		first = hb;
+		second = ha;
+	}
+
+	p = first;
+	while (p->next)
+	{
+		p = p->next;
+	}
+
+	if (second->next)
+	{
+		p->next = second->next;
+	}
+
+	*hc = first;
+	return 0;
+}
--- linear_table/linklist.h
+++ linear_table/linklist.h
@@ -12,4 +12,13 @@ void destroy_linklist(linklist head);
 int insert_node(linklist head, linknode *node);
 void linklist_traverse(linklist head);
 
+/* LOCATE */
+int linklist_locate(linklist l, int x);
+
+/* LENGTH */
+int linklist_length(linklist l);
+
+/* link two list as one */
+int linklist_link_two_list(linklist ha, int la, linklist hb, int lb, linklist *hc);
+
 #endif
